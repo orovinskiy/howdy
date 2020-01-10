59416bd35f8d69c04d6110e7185e5d0d7d6f8e55
Oleg Rovinskiy <orovinskiy@mail.greenriver.edu>
2020-01-10 13:37:23 -0500

    Added a main page to be displayed

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 $f3->route('GET /', function(){
-    echo "<p>Hello WOrld</p>";
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 $f3->run();
